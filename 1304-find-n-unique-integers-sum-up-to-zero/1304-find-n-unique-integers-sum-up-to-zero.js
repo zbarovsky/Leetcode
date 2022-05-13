48a36cc6421ada2e956491aee2703a87e85a3f5a
Zack Barovsky <zachary.barovsky@gmail.com>
2022-05-13 08:55:21 -0700

    Time: 69 ms (79.50%), Space: 42.8 MB (26.74%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -16,3 +16,31 @@ var sumZero = function(n) {
     return output
     
 };
+
+// var sumZero = function(n) {
+//     let output = [];
+    
+//     if(n % 2 == 0) {
+//         for(let i = 0; i < n; i++) {
+//             let num;
+//             if(i % 2 == 0) {
+//                 num = -i - 1
+//                 output.push(num)
+//             } else {
+//                 num = i
+//                 output.push(num)
+//             }
+//         }
+//     } else {
+//         for(let i = 0; i < n; i++) {
+//             let num;
+//             if(i % 2 == 0) {
+//                 num = i
+//                 output.push(num)
+//             } else {
+//                 num = -i - 1
+//                 output.push(num)
+//             }
+//         }
+//     }
+//     return output
