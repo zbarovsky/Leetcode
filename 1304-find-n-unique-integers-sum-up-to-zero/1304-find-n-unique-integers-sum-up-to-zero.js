2fcbea88aed92e0b68f4a09b77f8589aaaeb5e0d
Zack Barovsky <zachary.barovsky@gmail.com>
2022-05-13 08:53:07 -0700

    Time: 131 ms (5.70%), Space: 43 MB (12.30%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -5,28 +5,13 @@
 var sumZero = function(n) {
     let output = [];
     
-    if(n % 2 == 0) {
-        for(let i = 0; i < n; i++) {
-            let num;
-            if(i % 2 == 0) {
-                num = -i - 1
-                output.push(num)
-            } else {
-                num = i
-                output.push(num)
-            }
-        }
-    } else {
-        for(let i = 0; i < n; i++) {
-            let num;
-            if(i % 2 == 0) {
-                num = i
-                output.push(num)
-            } else {
-                num = -i - 1
-                output.push(num)
-            }
-        }
+    for(let i = 1; i < Math.floor(n/2)+1; i++) {
+        output.push(i);
+        output.push(-i);
+    }
+    
+    if(n % 2 != 0) {
+        output.push(0)
     }
     return output
     
