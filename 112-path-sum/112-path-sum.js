1a3525157ecd667c5b47fda894321e744c0907ea
Zack Barovsky <zachary.barovsky@gmail.com>
2022-06-05 20:42:22 -0700

    Time: 105 ms (38.08%), Space: 45.4 MB (73.74%) - LeetHub

--- 112-path-sum/112-path-sum.js
+++ 112-path-sum/112-path-sum.js
@@ -25,13 +25,11 @@ function traverse(root, sum, target) {
         return false
     }
     
-    sum += root.val
-    
     if(!root.left && !root.right) {
-        return sum == target;
+        return sum + root.val == target;
     }
     
-    
+    sum += root.val
     
     return traverse(root.left, sum, target) || traverse(root.right, sum, target)
 
