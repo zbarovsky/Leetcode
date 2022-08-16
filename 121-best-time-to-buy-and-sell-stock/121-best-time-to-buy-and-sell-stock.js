473f75e07c07eb0ba320c2e1c827cdd6cdd38ed0
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-15 20:49:52 -0700

    Time: 119 ms (59.58%), Space: 51.4 MB (91.17%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,7 +4,6 @@
  */
 var maxProfit = function(prices) {
     let low = prices[0];
-    let lowInt = 0;
     
     let high = prices[0];
     let highInt = 0;
@@ -15,7 +14,6 @@ var maxProfit = function(prices) {
         
         if(prices[i] < low) {
             low = prices[i];
-            lowInt = i
             
             if(i > highInt) {
                 high = 0;
