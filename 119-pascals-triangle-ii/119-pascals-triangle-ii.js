07c333719bf7dea4447162477d533802dbeb8262
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-17 16:51:31 -0700

    Time: 60 ms (95.34%), Space: 41.7 MB (92.99%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -3,17 +3,20 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    let output = []
-    output.push(1)
+    let triangle = []
     
-    for(let i = 0; i < rowIndex; i++) {
+    for(let i = 0; i <= rowIndex; i++) {
+        let row = []
+        row[0] = 1
+        row[i] = 1
         
-        for(let j = i; j > 0; j--) {
-            // sets inner numbers
-            output[j] += output[j - 1]
+        for(let j = 1; j < row.length - 1; j++) {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j] 
         }
-        // covers 0 and rowIndex which is always 1
-        output.push(1)
+        
+        triangle.push(row)
+        if(i == rowIndex) {
+            return row
+        }
     }
-    return output;
 };
