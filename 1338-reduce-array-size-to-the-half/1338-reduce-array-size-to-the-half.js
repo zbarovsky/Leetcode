609223721a5ef445e55ce335bb157573bf9ca953
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-18 14:16:23 -0700

    Time: 249 ms (43.40%), Space: 61.3 MB (48.11%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -4,34 +4,31 @@
  */
 var minSetSize = function(arr) {
     
-    let sizeArr = []
-    let count = 0;
-    let output = 0;
+    let hash = {}
+    let counter = 0;
     let halfArr = arr.length / 2
     let arrLen = arr.length
-    
-    arr = arr.sort((a,b) => a - b)
+    let output = 0;
     
     for(let i = 0; i < arr.length; i++) {
-        if(arr[i] === arr[i + 1]) {
-            count++
+        if(hash[arr[i]] != undefined) {
+            hash[arr[i]]++
         } else {
-            count++
-            sizeArr.push(count)
-            count = 0
+            hash[arr[i]] = 1
         }
     }
     
-    sizeArr = sizeArr.sort((a,b) => b - a)
+    let counts = Array.from(Object.values(hash))
     
-    for(let i = 0; i < sizeArr.length; i++) {
-        arrLen -= sizeArr[i]
+    counts = counts.sort((a, b) => b - a)
+    
+    for(count in counts) {
+        arrLen -= counts[count]
         output++
         if(arrLen <= halfArr) {
             return output
         }
     }
     
-    return output
     
 };
