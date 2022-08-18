c006786b9b7f58c10f423c23ba36b4b6d596d3df
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-18 15:04:54 -0700

    Time: 192 ms (14.81%), Space: 46.5 MB (69.33%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    
+    let counter = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === 0) {
+            nums.splice(i,1)
+            counter++
+            i--
+        }
+    }
+    
+    for(let i = 0; i < counter; i++) {
+        nums.splice(nums.length, 0, 0)
+    }
+
+};
