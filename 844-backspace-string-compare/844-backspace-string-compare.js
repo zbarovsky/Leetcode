8347ba1c626d34496d94a9e0ede0d8c9ce86ccf8
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-22 16:39:14 -0700

    Time: 145 ms (5.10%), Space: 47.1 MB (5.17%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    
+    let sStack = []
+    let tStack = []
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === "#") {
+            sStack.pop()
+        } else {
+            sStack.push(s[i])
+        }
+    }
+    
+    for(let i = 0; i < t.length; i++) {
+        if(t[i] === "#") {
+            tStack.pop()
+        } else {
+            tStack.push(t[i])
+        }
+    }
+    
+    console.log(sStack, tStack)
+    
+    return JSON.stringify(sStack) == JSON.stringify(tStack)
+    
+    
+};
