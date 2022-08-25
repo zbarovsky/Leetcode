105ff046114e4e04f04fe98a9d7924738df15f49
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-25 07:07:21 -0700

    Time: 1781 ms (5.00%), Space: 48.5 MB (5.00%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,6 +3,14 @@
  * @return {number}
  */
 var fib = function(n) {
+    let cache = {}
+    
+    if(cache[n] != undefined) {
+        return cache[n]
+    } else {
+        cache[n] = n
+    }
+    
     if(n < 2) return n;
     
     return fib(n - 1) + fib(n - 2);
