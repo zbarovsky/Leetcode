4ee4f57b903225fbfb8df597f96701f3ee561b75
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-28 12:14:57 -0700

    Time: 158 ms (48.50%), Space: 45.4 MB (55.63%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -3,9 +3,6 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    
-    let queue = []
-    let length;
     let hash = {}
     
     for(let i = 0; i < s.length; i++) {
@@ -15,16 +12,11 @@ var firstUniqChar = function(s) {
         } else {
             hash[s[i]] = [i, 1]
         }
-        
-        queue.push(s[i])
     }
     
-    length = queue.length
-    
-    for(let i = 0; i < length; i++) {
-        let dequeued = queue.shift();
-        if(hash[dequeued][1] === 1) {
-            return hash[dequeued][0]
+    for(val in hash) {
+        if(hash[val][1] === 1) {
+            return hash[val][0]
         }
     }
     
