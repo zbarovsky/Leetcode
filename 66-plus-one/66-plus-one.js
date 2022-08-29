04ba4f089ab17fc8713901d2d6bd61ce1d7ba960
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-28 19:47:29 -0700

    Time: 109 ms (15.75%), Space: 42.1 MB (39.57%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    if(digits[digits.length - 1] == 9) {
+        for(let i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++
+                return digits
+            } else if (digits[i] === 9) {
+                digits[i] = 0
+            }
+            
+            if(i === 0) {
+                digits.unshift(1)
+            }
+        }
+    } else {
+        digits[digits.length - 1]++
+    }
+    
+    return digits
+    
+};
