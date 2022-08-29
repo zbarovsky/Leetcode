72a100ac77b06ab2bb97bacf3c1cfe1eab79baef
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-28 21:11:56 -0700

    Time: 114 ms (17.78%), Space: 42.9 MB (27.94%) - LeetHub

---
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} number
+ * @param {character} digit
+ * @return {string}
+ */
+var removeDigit = function(number, digit) {
+    let maxOutput = 0;
+    
+    for(let i = 0; i < number.length; i++) {
+        
+        if(number[i] == digit) {
+            let tString = number.slice(0, i) + number.slice(i+1)
+            
+            if(tString > maxOutput) {
+                maxOutput = tString
+            }
+        }
+        
+    }
+    
+    return maxOutput
+};
