57a971256f6720e783186440b4d0243969c823fa
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-29 20:13:42 -0700

    Time: 92 ms (76.09%), Space: 47.6 MB (22.10%) - LeetHub

---
+++ 136-single-number/136-single-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let hash = {}
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(hash[nums[i]] != undefined) {
+            hash[nums[i]]++
+        } else {
+            hash[nums[i]] = 1
+        }
+    }
+    
+    for(num in hash) {
+        if(hash[num] === 1) {
+            return num
+        } 
+    }
+    
+};
