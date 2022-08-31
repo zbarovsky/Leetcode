f1d51c80100f99aa62168270fda3b2c16febaea5
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-30 21:02:25 -0700

    Time: 161 ms (49.18%), Space: 48.6 MB (69.66%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -6,5 +6,6 @@ var sortedSquares = function(nums) {
     for(let i = 0; i < nums.length; i++) {
         nums[i] = nums[i] * nums[i]
     }
-    return nums.sort((a, b) => a - b)
+    
+    return nums.sort((a,b) => a - b)
 };
