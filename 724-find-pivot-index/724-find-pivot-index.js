a05bd2f85ea6df145a097dd365f2889391635b9f
Zack Barovsky <zachary.barovsky@gmail.com>
2022-09-01 15:38:56 -0700

    Time: 136 ms (38.52%), Space: 44.3 MB (73.03%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let sum = 0;
+    let leftSum = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+    }
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(leftSum == (sum - nums[i] - leftSum)) {
+            return i
+        } else {
+            leftSum += nums[i]
+        }
+    }
+    
+    return -1
+    
+};
