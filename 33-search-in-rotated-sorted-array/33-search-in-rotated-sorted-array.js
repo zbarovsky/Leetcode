6b9a503067b800e206a0785694ec6524a6c393b0
Zack Barovsky <zachary.barovsky@gmail.com>
2022-08-31 20:41:59 -0700

    Time: 109 ms (18.25%), Space: 41.8 MB (86.34%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length-1;
+    
+    while(start <= end) {
+        if(nums[start] === target) {
+            return start
+        } else if (nums[end] === target) {
+            return end
+        } else {
+            start++
+            end--
+        }
+    }
+    return -1
+};
